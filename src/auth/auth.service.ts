e4a663a8f542c3edb5b4fa9044e05e7a08d603b1
Hafeez Ghanchi <Hafeezghanchi927@gmail.com>
2023-02-12 22:03:25 +0500

    added user basic signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { AuthDto } from './dto';
+import * as argon2 from 'argon2';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
-  signup() {
-    return { msg: 'signup service called' };
+  constructor(private prisma: PrismaService) {}
+
+  async signup(dto: AuthDto) {
+    const email = dto.email;
+    const hash = await argon2.hash(dto.password);
+
+    const user = await this.prisma.user.create({
+      data: { email, hash },
+    });
+
+    return user;
   }
+
   signin() {
     return { msg: 'signin service called' };
   }
