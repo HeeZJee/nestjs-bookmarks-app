d28d5124241f4082256215b8fa555647a7866588
Hafeez Ghanchi <Hafeezghanchi927@gmail.com>
2023-02-12 22:54:01 +0500

    handled duplicated email entries

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,21 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { AuthDto } from './dto';
 import * as argon2 from 'argon2';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
   async signup(dto: AuthDto) {
-    const email = dto.email;
-    const hash = await argon2.hash(dto.password);
+    try {
+      const email = dto.email;
+      const hash = await argon2.hash(dto.password);
 
-    const user = await this.prisma.user.create({
-      data: { email, hash },
-    });
+      const user = await this.prisma.user.create({
+        data: { email, hash },
+      });
 
-    return user;
+      delete user.hash;
+
+      return user;
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+        }
+        throw new ForbiddenException('Credentials taken');
+      }
+      throw error;
+    }
   }
 
   signin() {
